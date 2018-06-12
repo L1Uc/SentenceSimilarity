12786132457c7fc260a40730b29b8038ab720a20
L1Uc <m13261632677@163.com>
2018-06-12 21:00:41 +0800

    Add files via upload

---
+++ LDA_16000/evaluate.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+import os,glob
+import codecs
+
+if __name__ == "__main__":
+    correct_id = []
+    result_id = []
+    count_id = []
+    i = -1
+    accuracy = 1
+    f_label = open("label_lc_16000.txt","r")
+    f_result = open("result_1.txt")
+    
+    for eachline1 in f_label.readlines():
+        i += 1
+        if eachline1[0] == "1":
+            correct_id.append(str(i))
+        else :
+            continue
+        
+    for eachline2 in f_result.readlines():
+        result_list = eachline2.split()
+        result_id.append(result_list[0])
+
+    f_label.close()
+    f_result.close()
+    
+    # 第一行数据处理
+    result_id[0] = '0'
+    correct_id.insert(0,'0')
+    
+    # 将字符串列表转为整型列表
+    result_id = map(eval,result_id)
+    correct_id = map(eval,correct_id)
+    
+    # result_id再处理
+    # for j in range(0,160):
+        # for k in range(0,5):
+            # result_id[k+j*5] = result_id[k+j*5] + j*100
+    
+    for j in range(0,160):
+        result_id[j] = result_id[j] + j*100
+    
+    # 得到相同元素及个数
+    count_id = [item for item in correct_id if item in result_id]
+    print "number of same item: ",len(count_id)
+    
+    # 计算准确率
+    accuracy = float(len(count_id))/float(800)
+    print "accuracy: ",accuracy
+    print '\n'
+    
+    print "correct_id = ",correct_id
+    print '\n'
+    print "result_id = ",result_id
+    print '\n'
+    print "count_id = ",count_id
+    
+    
+    
